1cd7012b3ffca36792ac2fb342716abc136a1343
Rümeysa Ceylan <59581456+RumeysaCeylan@users.noreply.github.com>
2022-05-13 19:09:58 +0300

    Add files via upload

---
+++ src/DataBase/DbHelper.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataBase;
+
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+/**
+ *
+ * @author Rumeysa
+ */
+public class DbHelper {
+    private String userName="root";
+    private  String password="Rumeysa06.";
+    private String port="jdbc:mysql://localhost:3306/devwomen";
+    
+    public Connection getConnection()throws SQLException{
+        
+        return DriverManager.getConnection(port,userName,password);
+        
+    }
+    public void showErrorMessage(SQLException e){
+        System.out.println("DataBase.DbHelper.showErrorMessage() "+e.getMessage() +" "+e.getErrorCode());
+    }
+
+  
+
+}
---
+++ src/DataBase/DbWrite.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import Person.Entrepreneur;
+
+/**
+ *
+ * @author Rumeysa
+ */
+public class DbWrite {
+    DbHelper db = new DbHelper();
+    Connection connect = null;
+    PreparedStatement statement = null;
+    Statement statement2 = null;
+    ResultSet resultset;
+    public void addEntAccount(Entrepreneur entrepreneur){
+       
+        try {
+            connect=db.getConnection();
+             String query="INSERT INTO USERS(Area,UserName,Email,UserPassword) VALUES(?,?,?,?)";
+             statement=connect.prepareStatement(query);
+             statement.setString(1, entrepreneur.getWorkingArea());
+             statement.setString(2,entrepreneur.getUserName());
+             statement.setString(3,entrepreneur.getEmail());
+             statement.setString(4,entrepreneur.getPassword());
+             statement.executeUpdate();
+            System.out.println("connected");
+        } catch (SQLException e) {
+            System.out.println( e.getMessage());
+    }
+        finally{
+            try {
+                statement.close();
+                connect.close();
+            } catch (SQLException e) {
+                System.out.println("DataBase.DbWrite.addEntAccount()");
+            }
+        }
+        }
+   
+}
+
+        
